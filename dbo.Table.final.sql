707b4ec525c294d15c47389230d0378c16a55ffe
Heather L Uebel <tessouhaits@gmail.com>
2019-03-29 06:36:54 -0400

    Add files via upload
    You need this sql file to run the queries

---
+++ dbo.Table.final.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Name]   NVARCHAR (50)  NOT NULL,
+    [Phone]  NVARCHAR (50)  NULL,
+    [Street] NVARCHAR (MAX) NULL,
+    [City]   NVARCHAR (50)  NULL,
+    [State]  NVARCHAR (50)  NULL,
+    [Zip]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Table] PRIMARY KEY CLUSTERED ([Name] ASC)
+);
+
